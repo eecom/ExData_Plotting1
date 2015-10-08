7dc0a32ff9d070cdb375f08658ec67f198f64dd3
eecom <kasemc@gmail.com>
2015-10-08 20:15:25 +0700

    Add Plot2 code

--- plot2.R
+++ plot2.R
@@ -1,1 +1,8 @@
-﻿A
+alldata = read.table("household_power_consumption.txt", sep = ";", header=TRUE, na.strings="?")
+data <- subset(alldata, Date == "1/2/2007" | Date == "2/2/2007")
+data$DateTime = strptime(paste(as.character(data$Date), as.character(data$Time), sep = " "),"%d/%m/%Y %H:%M:%S")
+
+windows(5,5)
+with(data, plot(DateTime, Global_active_power, type="n", xlab="", ylab="Global Active Power (kilowatts)"))
+lines(data$DateTime, data$Global_active_power, type="l")
+
